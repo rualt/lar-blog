4314819a398918d65cba52a0ba6433f48775f586
rualt <>
2023-11-23 22:06:47 +0400

    Add initial blog view and router

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+            <h1><a href="/post">Post 1</a></h1>
+
+            <p>
+                Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+                Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+                Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+                Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+            </p>
+
+            <p>
+                Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+                Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+                Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+                Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+            </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Post 2</a></h1>
+
+        <p>
+            Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+            Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+            Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+            Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+        </p>
+
+        <p>
+            Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+            Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+            Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+            Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Post 3</a></h1>
+
+        <p>
+            Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+            Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+            Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+            Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+        </p>
+
+        <p>
+            Deserunt cillum sint aliqua laborum minim nostrud commodo consequat aliqua dolore ullamco.
+            Excepteur nostrud eiusmod proident fugiat. Officia fugiat nulla est eu laboris in cupidatat sint velit et minim.
+            Ea nulla cillum mollit velit esse dolore. Sint dolore sit voluptate id officia minim est qui.
+            Elit elit consectetur quis qui proident ullamco labore amet ea eu cillum fugiat. Ipsum laborum est sit nisi.
+        </p>
+    </article>
+</body>
+</html>
