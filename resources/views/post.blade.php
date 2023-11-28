f11e8a3afbf89e1f112f253aedff466effeca996
rualt <>
2023-11-28 23:35:59 +0400

    Add loyout blade component

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $post->title }}</title>
-
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     <article>
         <article>
             <h1>
@@ -21,5 +11,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts list</title>
-
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article>
             <h1>
@@ -21,5 +11,4 @@
             </p>
         </article>
     @endforeach
-</body>
-</html>
+</x-layout>
