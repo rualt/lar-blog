6dfcf80da4abeaf46b32908d5d3a8b350be036e4
rualt <>
2024-06-28 16:41:48 +0400

    Define relationship for Posts with Categories and Users, update template with links

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,11 +9,16 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id', 'user_id'];
     // protected $guarded = ['id'];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Post;
+use App\Models\{Category, Post, User};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 });
 
-Route::get('posts/{post}', function (Post $post) {
+Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
 })->where('post', '[A-z0-9_-]+');
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts->load(['category', 'author'])
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author'])
+    ]);
+});
