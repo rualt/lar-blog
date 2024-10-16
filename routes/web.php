18da8dc80de20c8eccb1e1201ef70319f98c7ba1
rualt <>
2024-10-16 22:32:53 +0400

    Add filter by author

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\{Category, Post, User};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-});
