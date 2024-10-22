ec96601fbdb4c7d710433307daa2eabe384d97fb
rualt <>
2024-10-22 20:29:12 +0400

    Fix search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,9 +17,10 @@ class Post extends Model
         $query->when(
             $filters['search'] ?? false,
             fn ($query, $search) =>
-                $query
-                    ->where('title', 'like', '%' . $search . '%')
+                $query->where(fn($query) =>
+                    $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('body', 'like', '%' . $search . '%')
+                )
         );
 
         $query->when(
