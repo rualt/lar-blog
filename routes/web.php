e73dd6dfc6dc6cd6a31b2467fc6f5949a254e6f8
rualt <>
2024-10-22 21:50:34 +0400

    Add basic user registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\{PostController, RegisterController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+
+Route::post('register', [RegisterController::class, 'store']);
